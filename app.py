dbde8353fe126bcb70add752b34cb98deb3ead53
Alvaro Rodriguez <arod.gzz28@gmail.com>
2020-08-01 19:28:29 -0500

    updated my branch with code modification
    I changed the nested list to a dictionary, and be able to put them on the page.

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
-import get_tweets
+from get_tweets import *
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home_page():
-    get_tweets()
-    return "home_page"
+@app.route('/<name>')
+def home_page(name):
+    listOfData = getTweets("#live","2017-04-03")
+    return listOfData
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- get_tweets.py
+++ get_tweets.py
@@ -10,43 +10,38 @@ auth.set_access_token(credentials.access_token,
 
 api = tweepy.API(auth)
 
-# This can be used for #s and ip of the user
-# returns dataframe with the info of the tweets
-def getTweets (keyWord):
+#public_tweets = api.home_timeline()
+#for tweet in public_tweets:
+#    print(tweet.text)
+
+def getTweets (keyWord, date):
     keyWord = keyWord + " -filter:retweets" # Filters retweets
     tweets = tweepy.Cursor(api.search,
-                       q=keyWord).items(5) # only returns 5 tweets, but it can be changed
-    # DataFrame with the users (string), the tweet (string), the tweet id (Int64)
-    # their location where the tweet was made (coordinates) and if the tweet is sensitive (boolean)
-    tweetsList = pd.DataFrame()
+                       q=keyWord,
+                       lang="en",
+                       since=date).items(20)
+    #tweetsList = []
+    tweetDict = {}
     for tweet in tweets:
-        tweetElement = [tweet.user.screen_name,tweet.text, tweet.id]
+        tweetId = tweet.id
+        tweetElement = [tweet.user.screen_name,tweet.text]
         try:
-            tweetElement.append(tweet.place.id) # If the user doesnt share their location
+            tweetElement.append(tweet.place_type.name)
         except:
             tweetElement.append(None)
         try:
-            tweetElement.append(tweet.possibly_sensitive) # This field only surfaces when a Tweet contains a link
+            tweetElement.append(tweet.possibly_sensitive)
         except:
             tweetElement.append(None)
-        tweet_serie = pd.Series({"user" : tweetElement[0],
-                                 "tweet" : tweetElement[1],
-                                 "tweetID" : tweetElement[2],
-                                 "location" : tweetElement[3],
-                                 "sensitive" : tweetElement[4]})
-        tweetsList = tweetsList.append(tweet_serie,ignore_index=True)
-    return tweetsList
+        #tweetsList.append(tweetElement)
+        tweetDict[tweetId] = tweetElement
 
-# function to get the text of each tweet for the other api we are using
-# returns list with the texts of wach tweet
-def getTweetsText(tweetsList):
-    texts = tweetsList["tweet"]
-    textList = []
-    for text in texts:
-        data = text.split("https://") # Separates the urls
-        textList.append(data[0])
-    return textList
+    # DataFrame with the users (string), the tweet (string),
+    # their location where the tweet was made (coordinates) and if the tweet is sensitive
+    # tweet_text = pd.DataFrame(data=tweetsList, columns=['user', "tweet", "location", "sensitive"])
+    return tweetDict
 
-# print(getTweets("74.125.19.104"))
-#print(getTweets("#cats"))
-#print(getTweetsText(getTweets("#cats")))
+
+
+listOfData = getTweets("#cats","2017-04-03")
+print(listOfData)
